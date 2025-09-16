4b44996e0c7418962519146968d289d9fc69ae9b
Jean-Philippe Brabant <JPBrabant@outlook.com>
2025-09-15 21:47:24 -0400

    Adding info about file and directory.

--- 2. PowerShell.ps1
+++ 2. PowerShell.ps1
@@ -35,6 +35,9 @@ Get-Process | Select-Object -First 1 -Property *
 # If you are looking for a command about object but you forgot it's name
 *object # CTRL+TAB will show all command ending with object
 
+# Figuring out the type of an object (don't forget most command return an array of object)
+(ps)[0].GetType().FullName
+
 #######################
 # FILES AND DIRECTORY #
 #######################
@@ -92,6 +95,9 @@ Get-Process | Measure-Object -Line # Number of line in a string
 Get-Process | Measure-Object -Character # Number of character in a string
 Get-Process | Measure-Object -Property Id -Minimum # Maximum, Sum, Average, ...
 
+# Measure the time it take to run a command. Piping to out-host allow you to see the result of the command.
+Measure-Command -Expression { Get-ChildItem -Recurse C:\Git | Out-Host } 
+
 # Filtering.
 Get-Process | Where-Object -Property PriorityClass -Value Normal -EQ # Long form
 Get-Process | Where-Object PriorityClass -EQ Normal                  # Short form
@@ -145,8 +151,21 @@ Install-Module SqlServer
 # COLLECTION AND DATATYPE #
 ###########################
 
-@(1,2,3) # Array
-@{Test = $true, $str = "String"} # Hash table
+$array = @()
+$array = @(1,2,3) # Array
+$array = 1,2,3
+$array = @( # No comma
+    1
+    2
+    3
+) # Array
+
+$hashTable = @{}
+$hasTable = @{Test = $true, $str = "String"} # Hash table
+$hasTable = @{ # No comma
+    KeyName1 = "Value 1"
+    KeyName2 = "Value 2"
+}
 
 # Custom object multiline
 [PSCustomObject]@{
@@ -157,7 +176,105 @@ Install-Module SqlServer
 # Custom object one line
 [PSCustomObject]@{Name="CustomObject";Expand=@(1,2,3,4,5)}
 
+# String multiline
+@"
+Text
+"@
+
 50MB # You can use MB, GB, KB, ...
+
+##########
+# STRING #
+##########
+
+"Lastname;FirstName;Address" -split ";" # There is other option for this command, see about_split.
+"John", "Smith" -join " "
+$_.Name -replace '\.txt$','.log'
+
+# https://learn.microsoft.com/dotnet/api/system.string.format
+"Value {0} inside a string" -f $value
+"Price is {0:C2}" -f $dollarValue # Format as a currency
+
+# Clipboard
+Get-Content 'test.txt' | Set-Clipboard
+Get-Clipboard
+
+########
+# DATE #
+########
+
+Get-Date
+Get-Date + New-TimeSpan -Hour 6
+(Get-Date).AddDays(2)
+
+
+#########
+# REGEX #
+#########
+
+'' -match ''
+'' -replace '', ''
+Select-String -Pattern ''
+
+# Visual representation of regex at : https://www.debuggex.com/
+
+# . : Any character
+# a* : Zero or more time
+# b+ : One or more time
+# c{2} : 2 times
+# c{2,4} : 2 to 4 times
+# c{2,} : minimum 2 times
+# d? : 0 or 1 time
+# All previous quantifier are greedy, they can be made lazy with .*?
+# \ : Escape special char
+# Non printable char : \t (tab), \n\r (line feed and carriage return)
+# [abc]
+# [a-c]
+# [^aeiou] : Not a vowel
+# \d or \D : Digit or not a digit
+# \w or \W : Word character (a-z, 0-9, _) or not a word character
+# \s or \S : White space
+
+# one|two : OR
+# (one|two) : group
+
+# Automatic variable
+'The first rule is' -match 'first'
+$Matches # Contain an array with [0] = 'first'
+
+# $Matches[0] : Alway contain the whole match
+# $Matches[1] : If using group, they will show up after 1
+# (?<GroupName>regex) : Named capture group $Matches["GroupName"]
+
+# Achors
+'C:\Test' -match '^C' # Start with C
+'C:\Test' -match 'Test$' # End with test
+'The first day is monday' -replace '\bday\b', 'night' # \b is a Word bondary, it wont replace the day inside monday.
+
+# Look ahead and look behind
+# Describe what happen before or after an expression without affecting what is captures in the $Matches array
+"includedRegex(?=, lookAheadRegex)"
+"(?<=, lookBehindRegex)includedRegex"
+
+# Regex option
+# i : case insensitive
+# m : multi-line
+# s : single-line
+# n : ignore unamed group
+# x : ignore unsacaped white space (allow you to use comment)
+
+# Exemple
+# x option allow comment
+# n ignore unamed group
+# /s and /s usefull to parse output of linux or DOS command like ls
+$regex = '(?xn)
+# Start of string
+^
+# Left hand side of the table
+\s*
+# First column
+(?<Protocol>\S*)\s+
+'
 
 ############
 # OPERATOR #
@@ -166,11 +283,46 @@ Install-Module SqlServer
 -and
 -or
 -not
-"Lastname:FirstName:Address" -split ":", 2, "SimpleMatch" # The separator, max number of instance and options (https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_split)
--replace
-"1" -as [Int32]
+
+"1" -as [Int32] # or simplit [Int32]"1" or [Xml]$newVar
 "1" -is [String]
 "1" -isnot [Int32]
+
+# Call operator : execute a string or script block
+& 'note.exe'
+
+# Background job operator
+$job = Get-Process &
+Get-Job
+$job | Receive-Job # Show the output of Receive-Job when finished.
+
+# Ternary operator
+$val = $valBool ? $trueVal : $falseVal
+
+# Null coalescing
+$val = $valueA ?? 'Default value' # If valueA is not null, assign valueA, if null assign 'Default value'
+
+# Chain operator
+program1.exe && program2.exe # Only run program 2 if program 1 did not fail (throw Exception).
+
+###############
+# FILE SYSTEM #
+###############
+
+Get-ChildItem # Need to use -Force if testing for hidden file
+Get-Item      # Need to use -Force if testing for hidden file
+New-Item
+Remove-Item
+Test-Path 
+Get-ItemProperty
+Set-ItemProperty
+Get-Content
+Get-Acl
+Set-Acl
+$tmpFile = New-TemporaryFile
+Set-Location # "Set-Location -" and "Set-Location +" allow you to go backward or foward in history
+Invoke-Item # Open .txt file with Notepad for example
+Get-FileHash -Algorithm SHA1 -Path .
 
 ##################
 # ERROR HANDLING #
@@ -182,13 +334,6 @@ try {
 catch {
     Write-Output $_.Exception.Message
 }
-
-##########
-# OTHERS #
-##########
-
-# grep.
-Select-String
 
 ##################
 # WHERE OPERATOR #
@@ -218,3 +363,60 @@ Select-String
 
 "Test" | Where-Object Length      # gets objects if the property exist and has a value
 "Test" | Where-Object -Not Length # gets objects if the property doesn't exist or has a value of $null or $false.
+
+###############
+# WEB REQUEST #
+###############
+
+# Best at dealing with HTML.
+# The parameter -UseBasicParsing is not used anymore.
+# Use Regex to parse or content or the custom module PSParseHTML.
+Invoke-WebRequest -Uri "https://evenko.ca/fr/salles/place-bell"
+
+# Best at dealing with JSON or XML.
+Invoke-RestMethod 
+
+####################
+# SPECIAL VARIABLE #
+####################
+
+# Home
+Set-Location ~
+Set-Location $HOME
+Set-Location $env:USERPROFILE
+
+# C:\Program Files
+Set-Location C:\progra~1 # 64 bits
+Set-Location C:\progra~2 # 32 bits
+Set-Location C:\progra~3 # C:\ProgramData
+
+#######
+# JOB #
+#######
+
+Start-Job -Name TestJob -ScriptBlock { Start-Sleep -Seconds 10 }
+Get-Job -Name TestJob | Receive-Job -Wait
+1..10 | ForEach-Object -Parallel { Write-Host $_ }
+
+###########
+# PROCESS #
+###########
+
+# It will open in the default program for that type of file. 
+# Alias : start
+Start-Process C:\Git # File explorer
+start test.txt # Notepad
+
+############
+# FUNCTION #
+############
+
+function New-Function {
+    param (
+         [String] $param1 = 'Default value',
+            [Int] $param2 = 1,
+        [Boolean] $param3 = $false
+    )
+
+}
+
